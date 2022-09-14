a7de0a1d779fff5ca6361324ed6fe0dd3d8e4259
PavelPodymov <poll.ppbpower@gmail.com>
2022-09-14 10:16:21 +0300

    Example 002 Hello User

---
+++ Example002_Hello_User/Program.cs
@@ -0,0 +1,13 @@
+﻿// Так записываем комментарий или "Ctrl+/"
+// Console.WriteLine("Hello User!");
+// Console.WriteLine("Введите ваше имя: ");
+// string username = Console.ReadLine();
+// Console.WriteLine("Привет, {username}");
+// Console.WriteLine(username);
+
+
+//  Выводит текст в одну строку
+Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
