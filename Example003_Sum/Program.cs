953b1f1a9f1e7641e32ece89aadac2aa7b7e2bb6
PavelPodymov <poll.ppbpower@gmail.com>
2022-09-14 10:53:11 +0300

    Creat Example003_Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// Так записываем комментарии
+// Console.WriteLine("Sum!");
+
+int numberA = 3;
+int numberB = 5;
+int sum = numberA + numberB;
+Console.WriteLine(numberA + numberB);
+Console.WriteLine(sum);
