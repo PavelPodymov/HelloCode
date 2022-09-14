e972c93430ff85e960403bc49d9ac44fd55e3ff1
PavelPodymov <poll.ppbpower@gmail.com>
2022-09-14 13:26:33 +0300

    Examle006_Base

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,19 @@
+﻿// Так записывается комментарий
+// Console.WriteLine("Base");
+
+int a = 1;
+int b = 2;
+int c = 12;
+int d = 8;
+int e = 4;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
